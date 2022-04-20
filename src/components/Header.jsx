116525089d5bcf9cb194f1478820dd23777ffbc9
Cristiano Vieira <cristianoviieira@gmail.com>
2022-04-19 21:33:28 -0300

    requisito 4 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -21,6 +22,9 @@ class Header extends React.Component {
       <header data-testid="header-component">
         <h1>TrybeTunes</h1>
         <p data-testid="header-user-name">{ nickName }</p>
+        <Link to="/search" data-testid="link-to-search"> search </Link>
+        <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
+        <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
       </header>
     );
   }
